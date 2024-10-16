a1d3f7d6b307184621a37c56c46f9daafc328609
FirdanAja <159457668+FirdanAja@users.noreply.github.com>
2024-10-16 23:21:44 +0700

    test

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-20 py-5 backdrop-blur fixed left-0 right-0 top-0 z-50">
+    <nav className="flex justify-between items-center px-20 py-3 backdrop-blur fixed left-0 right-0 top-0 z-50">
       <div className="flex items-center">
         <img src="/src/assets/img/logo.png" alt="Logo" className="h-8 mr-2" />
         <h2 className="font-bold text-xl">Ekraf Kuningan</h2>
       </div>
       <div>
-        <ul className="flex gap-5 items-center">
+        <ul className="flex gap-5 items-center font">
           <li>
             <a href="#" className="text-sm">
-              Beranda.
+              Beranda
             </a>
           </li>
           <li>
--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,13 +1,110 @@
 export default function HeaderHomeSection() {
   return (
-    <div className="bg-[url(https://images.unsplash.com/photo-1505416795390-0beeb662b1f2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] relative h-screen w-screen bg-cover bg-center after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-t after:from-black/60 after:to-black/0 flex items-center justify-center">
-      <div className="relative z-10">
-        <img
-          src="https://kuninganbeu.kuningankab.go.id/images/logo/KuninganBeu_Putih.png"
-          alt="Kuningan Beu Logo"
-          className="w-[200px]"
-        />
+    <>
+      <div className="bg-[url(https://images.unsplash.com/photo-1505416795390-0beeb662b1f2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] relative h-screen w-screen bg-cover bg-center after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-t after:from-black/60 after:to-black/0 flex items-center justify-center">
+        <div className="relative z-10">
+          <img
+            src="https://kuninganbeu.kuningankab.go.id/images/logo/KuninganBeu_Putih.png"
+            alt="Kuningan Beu Logo"
+            className="w-[500px]"
+          />
+        </div>
       </div>
-    </div>
+      <div className="subsector pt-20">
+        <h1 className="text-center lg:text-5x1 text-3xl font-medium font-semibold mb-2">
+          17 SUB SEKTOR
+        </h1>
+        <p className="text-center font-medium mb-10">
+          Badan Usaha Di Kabupaten Kuningan
+        </p>
+      </div>
+      <div className="grid grid-cols-4 gap-5 px-20">
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+        <div className="p-5 shadow-lg w-full bg-slate-50 rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:bg-slate-300 text-center">
+          <i className="fa-solid fa-palette text-3xl"></i>
+          <p className="card-text">Seni Pertunjukan</p>
+          <p className="text-gray-500">Sudah Ada - Sektor</p>
+        </div>
+      </div>
+    </>
   );
 }
